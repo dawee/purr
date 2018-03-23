8c8aa952b9ef60973259d0a5d16e0943872ac107
David Corticchiato <david.corticchiato@gmail.com>
2018-03-23 21:59:04 +0100

    open sdl window

--- src/purr.cc
+++ src/purr.cc
@@ -1,3 +1,4 @@
+#include <SDL2/SDL.h>
 #include <v8/v8.h>
 #include <v8/libplatform/libplatform.h>
 #include <filesystem/path.h>
@@ -5,31 +6,64 @@
 
 #include "project.h"
 
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
 int main(int argc, char * argv[]) {
   if (argc != 2) {
-     printf("usage:\n\tpurr <filename>\n");
-     return 1;
-   }
+    printf("usage:\n\tpurr <filename>\n");
+    return 1;
+  }
 
-   filesystem::path filePath(argv[1]);
+  SDL_Window* window = SDL_CreateWindow(
+    "Purr",
+    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+    SCREEN_WIDTH, SCREEN_HEIGHT,
+    SDL_WINDOW_SHOWN
+  );
 
-   v8::Platform* platform = v8::platform::CreateDefaultPlatform();
-   v8::V8::InitializePlatform(platform);
-   v8::V8::Initialize();
-   v8::Isolate::CreateParams create_params;
-   create_params.array_buffer_allocator =
-   v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+  if (window == NULL) {
+    fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+    return 1;
+  }
 
-   v8::Isolate* isolate = v8::Isolate::New(create_params);
-   {
-     v8::Isolate::Scope isolate_scope(isolate);
-     v8::HandleScope handle_scope(isolate);
-     purr::Project::Instance()->Require(filePath.make_absolute().str());
-   }
+  SDL_SetWindowBordered(window, SDL_TRUE);
+  SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 0, 0));
+  SDL_UpdateWindowSurface(window);
 
-   v8::V8::Dispose();
-   v8::V8::ShutdownPlatform();
-   delete create_params.array_buffer_allocator;
+  filesystem::path filePath(argv[1]);
 
-   return 0;
- }
+  v8::Platform* platform = v8::platform::CreateDefaultPlatform();
+  v8::V8::InitializePlatform(platform);
+  v8::V8::Initialize();
+  v8::Isolate::CreateParams create_params;
+  create_params.array_buffer_allocator =
+  v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+
+  v8::Isolate* isolate = v8::Isolate::New(create_params);
+  {
+    v8::Isolate::Scope isolate_scope(isolate);
+    v8::HandleScope handle_scope(isolate);
+    purr::Project::Instance()->Require(filePath.make_absolute().str());
+
+    SDL_Event event;
+    bool quit = false;
+
+    while (!quit) {
+      while (SDL_PollEvent(&event) != 0) {
+        if (event.type == SDL_QUIT || (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE)) {
+          SDL_HideWindow(window);
+          quit = true;
+        }
+      }
+    }
+  }
+
+  v8::V8::Dispose();
+  v8::V8::ShutdownPlatform();
+  delete create_params.array_buffer_allocator;
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
