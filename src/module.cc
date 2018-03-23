1ef3647a3175bb610b69012d523a0e5940b332af
David Corticchiato <david.corticchiato@gmail.com>
2018-03-23 23:01:22 +0100

    remove static callbacks

--- src/module.cc
+++ src/module.cc
@@ -8,6 +8,20 @@
 #include "project.h"
 
 namespace purr {
+  static void ExportsGetter(v8::Local<v8::String> property, const v8::PropertyCallbackInfo<v8::Value>& info) {
+    v8::Persistent<v8::Value> * exports = Project::Instance()->GetModuleFromRoot(info.Holder())->GetExports();
+    info.GetReturnValue().Set(*exports);
+  }
+
+  static void ExportsSetter(v8::Local<v8::String > property, v8::Local<v8::Value> value, const v8::PropertyCallbackInfo<void> &info) {
+    Project::Instance()->GetModuleFromRoot(info.Holder())->SetExports(value);
+  }
+
+  static void ModuleGetter(v8::Local<v8::String> property, const v8::PropertyCallbackInfo<v8::Value>& info) {
+    v8::Local<v8::Object> module = info.Holder();
+    info.GetReturnValue().Set(module);
+  }
+
   Module::Module(
     v8::Isolate * isolate,
     std::string filename
@@ -31,8 +45,9 @@ namespace purr {
     scriptFile.close();
 
     v8::Local<v8::ObjectTemplate> moduleTemplate = v8::ObjectTemplate::New(isolate);
-    moduleTemplate->SetAccessor(v8::String::NewFromUtf8(isolate, "exports"), &Module::ExportsGetter, &Module::ExportsSetter);
-    moduleTemplate->SetAccessor(v8::String::NewFromUtf8(isolate, "module"), &Module::ModuleGetter);
+
+    moduleTemplate->SetAccessor(v8::String::NewFromUtf8(isolate, "exports"), &ExportsGetter, &ExportsSetter);
+    moduleTemplate->SetAccessor(v8::String::NewFromUtf8(isolate, "module"), &ModuleGetter);
 
     v8::Local<v8::Context> context = v8::Context::New(isolate, NULL, moduleTemplate);
     v8::Context::Scope context_scope(context);
@@ -76,20 +91,6 @@ namespace purr {
     std::string strValue(*utf8Value);
 
     return strValue;
-  }
-
-  void Module::ExportsGetter(v8::Local<v8::String> property, const v8::PropertyCallbackInfo<v8::Value>& info) {
-    v8::Persistent<v8::Value> * exports = Project::Instance()->GetModuleFromRoot(info.Holder())->GetExports();
-    info.GetReturnValue().Set(*exports);
-  }
-
-  void Module::ExportsSetter(v8::Local<v8::String > property, v8::Local<v8::Value> value, const v8::PropertyCallbackInfo<void> &info) {
-    Project::Instance()->GetModuleFromRoot(info.Holder())->SetExports(value);
-  }
-
-  void Module::ModuleGetter(v8::Local<v8::String> property, const v8::PropertyCallbackInfo<v8::Value>& info) {
-    v8::Local<v8::Object> module = info.Holder();
-    info.GetReturnValue().Set(module);
   }
 
 }
