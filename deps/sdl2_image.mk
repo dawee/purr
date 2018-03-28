e226f9bfaf97df87e619a0df8b2d524c45833281
David Corticchiato <david.corticchiato@gmail.com>
2018-03-28 11:49:29 +0200

    wildcard on sdl2_image objects

--- deps/sdl2_image.mk
+++ deps/sdl2_image.mk
@@ -1,2 +1,19 @@
-release:
-	@echo "SDL_Image build not ready yet"
+UNAME := $(shell uname)
+SOURCES := $(wildcard *.c)
+
+ifeq ($(UNAME), Darwin)
+	SOURCES := ${SOURCES} $(wildcard *.m)
+endif
+
+OBJECTS := $(patsubst %.m,build/%_m.o,$(patsubst %.c,build/%_c.o,$(SOURCES)))
+
+build/%_m.o: %.m build
+	@echo $< $@
+
+build/%_c.o: %.c build
+	@echo $< $@
+
+build:
+	@mkdir -p build
+
+release: ${OBJECTS}
