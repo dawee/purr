f761de75ee9d9f4ed2ef8d9b988254c251c26c35
David Corticchiato <david.corticchiato@gmail.com>
2018-04-12 23:34:21 +0200

    fix feeder casting

--- src/feeder.cc
+++ src/feeder.cc
@@ -6,7 +6,7 @@ namespace purr {
   }
 
   void Feeder::FeedObject(const char * key, v8::Local<v8::Object> object) {
-    v8::Local<v8::Object> localRoot = v8::Local<v8::Object>::New(isolate, root);
+    v8::Local<v8::Value> localRoot = v8::Local<v8::Value>::New(isolate, root);
     object->Set(v8::String::NewFromUtf8(isolate, key), localRoot);
   }
 }
